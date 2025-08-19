5a29132e3e0b916259dd574490003e86556b9023
Jeremy Bush <jeremy.bush@zombor.net>
2025-08-19 07:16:28 -0500

    Move llm package to root

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -10,13 +10,14 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/zombor/gen/cmd/gen/config"
-	"github.com/zombor/gen/cmd/gen/llm"
-	"github.com/zombor/gen/cmd/gen/tui"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/google/generative-ai-go/genai"
 	"github.com/ollama/ollama/api"
 	"google.golang.org/api/option"
+
+	"github.com/zombor/gen/cmd/gen/config"
+	"github.com/zombor/gen/cmd/gen/tui"
+	"github.com/zombor/gen/llm"
 )
 
 func getShell() string {
